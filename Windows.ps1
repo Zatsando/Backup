56cf0dd94d19a478c68cc0fc6ff70eb514363406
Zacharias Vlatakis <zvracy13@gmail.com>
2021-11-09 14:42:00 +0200

    Update Windows.ps1

--- Windows.ps1
+++ Windows.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('<https://community.chocolatey.org/install.ps1>'));
 
-choco install firefox brave discord 7zip.install python3 git.install barrier greenshot intellijidea-ultimate jpegview nodejs-lts nvidia-geforce-now obs-studio onlyoffice origin parsec signal steam streamlink stremio virtualbox vlc vscode winfetch androidstudio amd-ryzen-chipset kdeconnect-kde -y;
+choco install waterfox brave discord 7zip.install python3 git.install barrier greenshot intellijidea-ultimate jpegview nodejs-lts nvidia-geforce-now obs-studio onlyoffice origin parsec signal steam streamlink stremio virtualbox vlc vscode winfetch androidstudio amd-ryzen-chipset kdeconnect-kde -y;
