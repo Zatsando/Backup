5f591cb9982df11d1bd5410eb6b89b533b49c282
Zacharias Vlatakis <zvracy13@gmail.com>
2021-11-23 14:27:36 +0200

    Update Windows.ps1

--- Windows.ps1
+++ Windows.ps1
@@ -1,3 +1,5 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('<https://community.chocolatey.org/install.ps1>'));
+#THIS INSTALLS THE CHOCO PACKET MANAGER
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'));
 
+#THIS INSTALLS ALL MY APPS
 choco install brave discord 7zip.install python3 git.install barrier sharex intellijidea-ultimate jpegview nodejs-lts nvidia-geforce-now obs-studio onlyoffice parsec signal steam streamlink stremio virtualbox vlc vscode winfetch androidstudio gsudo kdeconnect-kde -y;
