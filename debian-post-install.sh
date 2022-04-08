6a29ffeb220d37a46b0e3cc8074c60bd791d9704
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2022-04-08 17:09:45 +0300

    Update debian-post-install.sh

--- debian-post-install.sh
+++ debian-post-install.sh
@@ -16,15 +16,9 @@ flatpak remote-add flathub-beta https://flathub.org/beta-repo/flathub-beta.flatp
 sudo flatpak remote-delete flathub-beta; flatpak remote-add --user flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo;
 
 flatpak install flathub com.spotify.Client -y;
-flatpak install flathub org.signal.Signal -y;
-flatpak install flathub com.parsecgaming.parsec -y;
-flatpak install flathub com.stremio.Stremio -y;
 flatpak install flathub org.telegram.desktop -y;
-flatpak install flathub com.heroicgameslauncher.hgl -y;
 flatpak install flathub com.chatterino.chatterino -y;
 flatpak install flathub com.google.AndroidStudio -y;
-
-#work only
 flatpak install com.google.Chrome -y;
 flatpak install flathub com.anydesk.Anydesk -y;
 flatpak install flathub io.github.Figma_Linux.figma_linux -y;
@@ -48,6 +42,32 @@ sudo apt autoremove && sudo apt autoclean -y;
 wget https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors_amd64.deb;
 sudo dpkg -i onlyoffice-desktopeditors_amd64.deb;
 
+#other apps apt
+wget https://builds.parsecgaming.com/package/parsec-linux.deb;
+sudo dpkg -i parsec-linux.deb;
+
+wget https://dl.strem.io/shell-linux/v4.4.142/stremio_4.4.142-1_amd64.deb;
+sudo dpkg -i stremio_4.4.142-1_amd64.deb;
+
+wget https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb;
+sudo dpkg -i protonvpn-stable-release_1.0.1-1_all.deb;
+sudo apt-get update;
+sudo apt install protonvpn -y;
+sudo apt install gnome-shell-extension-appindicator gir1.2-appindicator3-0.1 -y;
+
+sudo add-apt-repository ppa:papirus/papirus;
+sudo apt-get update;
+sudo apt-get install papirus-icon-theme -y;
+
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+sudo apt update && sudo apt install signal-desktop;
+
+wget https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.2.6/heroic_2.2.6_amd64.deb;
+sudo dpkg -i heroic_2.2.6_amd64.deb;
+
 #latest wine
 sudo dpkg --add-architecture i386
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
@@ -56,21 +76,9 @@ sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ impish ma
 sudo apt update
 sudo apt install --install-recommends winehq-stable
 
-#protonvpn
-wget https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb;
-sudo dpkg -i protonvpn-stable-release_1.0.1-1_all.deb;
-sudo apt-get update;
-sudo apt install protonvpn -y;
-sudo apt install gnome-shell-extension-appindicator gir1.2-appindicator3-0.1 -y;
-
 #usefull appimages
 wget https://github.com/SevenTV/chatterino7/releases/download/v7.3.4/Chatterino-x86_64.AppImage;
 chmod +x Chatterino-x86_64.AppImage;
-
-#papirus icon-pack
-sudo add-apt-repository ppa:papirus/papirus;
-sudo apt-get update;
-sudo apt-get install papirus-icon-theme -y;
 
 #pipewire over pulseaudio (optional)
 sudo apt update;
