4f0fe00394f672e28303207ac147cbdf93a44715
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2022-06-25 13:16:01 +0300

    Create scoop.ps1

---
+++ scoop.ps1
@@ -0,0 +1,46 @@
+scoop bucket add extras;
+scoop install sharex;
+scoop install vlc;
+scoop install discord;
+scoop install signal;
+scoop install krita;
+scoop install obs-studio;
+scoop install anydesk;
+scoop install qbittorrent;
+scoop install stremio;
+scoop install thunderbird;
+scoop install streamlink;
+scoop install brave;
+scoop install git;
+scoop install firefox;
+scoop install kdeconnect;
+scoop install figma;
+scoop install eartrumpet;
+scoop install vncviewer;
+scoop install quick-picture-viewer;
+scoop install revouninstaller;
+scoop install onlyoffice-desktopeditors;
+scoop install vcredist2022;
+
+
+scoop bucket add main;
+scoop install vscode-insiders;
+scoop install 7zip;
+scoop install nodejs-lts;
+
+scoop bucket add versions;
+scoop install steam;
+scoop install python310;
+
+scoop bucket add nonportable;
+scoop install wireguard-np;
+scoop install protonvpn-np;
+scoop install wireguard-np;
+
+scoop bucket add java;
+scoop install oraclejre8;
+scoop install corretto11-jdk;
+
+//nonofficial
+scoop bucket add niaojin0407_scoop-niaojin https://github.com/niaojin0407/scoop-niaojin;
+scoop install parsec_win;
