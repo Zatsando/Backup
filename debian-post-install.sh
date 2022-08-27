63fcea35d0609e708cfd61e31bd2cbf107d9a775
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2022-08-27 23:00:17 +0300

    Update debian-post-install.sh

--- debian-post-install.sh
+++ debian-post-install.sh
@@ -32,7 +32,7 @@ sudo nala update &&
 sudo nala install --install-recommends winehq-devel -y;
 
 #flatpaks
-flatpak install flathub com.spotify.Client -y;flatpak install flathub org.telegram.desktop -y;flatpak install flathub com.chatterino.chatterino -y;flatpak install flathub com.anydesk.Anydesk -y;flatpak install flathub io.github.Figma_Linux.figma_linux -y;flatpak install flathub com.usebottles.bottles -y;flatpak install flathub org.onlyoffice.desktopeditors -y;flatpak install flathub org.signal.Signal -y;flatpak install flathub com.stremio.Stremio -y;flatpak install flathub com.heroicgameslauncher.hgl -y;flatpak install flathub com.brave.Browser -y;flatpak install flathub org.qbittorrent.qBittorrent -y;flatpak install flathub org.kde.krita -y;flatpak install flathub com.discordapp.Discord -y;flatpak install flathub com.valvesoftware.Steam -y;flatpak install flathub com.visualstudio.code -y;flatpak install flathub org.videolan.VLC -y;flatpak install flathub org.mozilla.Thunderbird -y;flatpak install flathub org.mozilla.firefox -y;
+flatpak install flathub com.github.bluesabre.darkbar -y;flatpak install flathub com.spotify.Client -y;flatpak install flathub org.telegram.desktop -y;flatpak install flathub com.chatterino.chatterino -y;flatpak install flathub com.anydesk.Anydesk -y;flatpak install flathub io.github.Figma_Linux.figma_linux -y;flatpak install flathub com.usebottles.bottles -y;flatpak install flathub org.onlyoffice.desktopeditors -y;flatpak install flathub org.signal.Signal -y;flatpak install flathub com.stremio.Stremio -y;flatpak install flathub com.heroicgameslauncher.hgl -y;flatpak install flathub com.brave.Browser -y;flatpak install flathub org.qbittorrent.qBittorrent -y;flatpak install flathub org.kde.krita -y;flatpak install flathub com.discordapp.Discord -y;flatpak install flathub com.valvesoftware.Steam -y;flatpak install flathub com.visualstudio.code -y;flatpak install flathub org.videolan.VLC -y;flatpak install flathub org.mozilla.Thunderbird -y;flatpak install flathub org.mozilla.firefox -y;
 
 #nodejs lts
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash;
@@ -48,10 +48,20 @@ wget https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb;
 sudo dpkg -i protonvpn-stable-release_1.0.1-1_all.deb;
 wget https://github.com/rustdesk/rustdesk/releases/download/1.1.9/rustdesk-1.1.9.deb;
 sudo dpkg -i rustdesk-1.1.9.deb;
-wget https://pokemmo.com/download_file/1/;
+wget https://github.com/Lafydev/wingpanel-indicator-ayatana/raw/master/com.github.lafydev.wingpanel-indicator-ayatana_2.0.8_odin.deb;
+sudo dpkg -i ./com.github.lafydev.wingpanel*.deb;
+wget https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb;
+sudo dpkg -i ./protonvpn-stable-release_1.0.1-1_all.deb;
+
+#wingpanel indicator
+mkdir -p ~/.config/autostart;
+cp /etc/xdg/autostart/indicator-application.desktop ~/.config/autostart/;
+sed -i 's/^OnlyShowIn.*/OnlyShowIn=Unity;GNOME;Pantheon;/' ~/.config/autostart/indicator-application.desktop;
+sudo nano /etc/xdg/autostart/indicator-application.desktop;
 
 #native packages
-sudo nala install gnome-shell-extension-manager gnome-shell-extension-appindicator gir1.2-appindicator3-0.1 wireguard synaptic android-tools-adb android-tools-fastboot streamlink python3 git gnome-tweaks ubuntu-restricted-extras neofetch openjdk-11-jdk -y;
+sudo apt update;
+sudo apt install protonvpn libwingpanel-dev indicator-application libglib2.0-dev libgranite-dev libindicator3-dev gnome-shell-extension-manager gnome-shell-extension-appindicator gir1.2-appindicator3-0.1 wireguard synaptic android-tools-adb android-tools-fastboot streamlink python3 git gnome-tweaks ubuntu-restricted-extras neofetch openjdk-11-jdk -y;
 
 #java
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64; 
