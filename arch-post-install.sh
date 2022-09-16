c32622ffe8b3b8fc159e2c34b4a227623ce6a4de
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2022-09-16 11:20:45 +0300

    ASDSAD

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -3,7 +3,7 @@
 #upgrade
 sudo pacman -Syyu -y;
 
-#uncomment repos
+#uncomment multilib
 sudo nano /etc/pacman.conf;
 
 #yay
@@ -13,20 +13,9 @@ cd yay;
 makepkg -si;
 
 #archrepo apps
-sudo pacman -S wine gst-plugin-gtk flatpak jre11-openjdk android-tools firefox-developer-edition gnome-shell-extension-appindicator qbittorrent virtualbox krita discord steam obs-studio vlc lutris streamlink python3 git gnome-tweaks neofetch barrier thunderbird -y;
+sudo pacman -S fuse wine gst-plugin-gtk flatpak jre11-openjdk android-tools gnome-shell-extension-appindicator streamlink python3 git gnome-tweaks neofetch -y;
 
 #flatpak apps
-flatpak install flathub com.spotify.Client -y;
-flatpak install flathub org.telegram.desktop -y;
-flatpak install flathub com.chatterino.chatterino -y;
-flatpak install flathub com.anydesk.Anydesk -y;
-flatpak install flathub io.github.Figma_Linux.figma_linux -y;
-flatpak install flathub com.ktechpit.wonderwall -y;
-flatpak install flathub org.onlyoffice.desktopeditors -y;
-flatpak install flathub org.signal.Signal -y;
-flatpak install flathub com.stremio.Stremio -y;
-flatpak install flathub com.heroicgameslauncher.hgl -y;
-flatpak install flathub com.brave.Browser -y;
-flatpak install flathub com.visualstudio.code -y;
+flatpak install flathub net.lutris.Lutris -y;flatpak install flathub com.github.bluesabre.darkbar -y;flatpak install flathub com.spotify.Client -y;flatpak install flathub org.telegram.desktop -y;flatpak install flathub com.chatterino.chatterino -y;flatpak install flathub com.anydesk.Anydesk -y;flatpak install flathub io.github.Figma_Linux.figma_linux -y;flatpak install flathub com.usebottles.bottles -y;flatpak install flathub org.onlyoffice.desktopeditors -y;flatpak install flathub org.signal.Signal -y;flatpak install flathub com.stremio.Stremio -y;flatpak install flathub com.heroicgameslauncher.hgl -y;flatpak install flathub com.brave.Browser -y;flatpak install flathub org.qbittorrent.qBittorrent -y;flatpak install flathub org.kde.krita -y;flatpak install flathub com.discordapp.Discord -y;flatpak install flathub com.valvesoftware.Steam -y;flatpak install flathub com.visualstudio.code -y;flatpak install flathub org.videolan.VLC -y;flatpak install flathub org.mozilla.Thunderbird -y;flatpak install flathub org.mozilla.firefox -y;
 
 wget https://github.com/rustdesk/rustdesk/releases/download/1.1.9/rustdesk-1.1.9-manjaro-arch.pkg.tar.zst;
--- archpantheon.sh
+++ archpantheon.sh
@@ -8,10 +8,25 @@ sudo pacman -S pantheon pantheon-session -y;
 
 sudo pacman -S lightdm lightdm-gtk-greeter lightdm-pantheon-greeter -y;
 
-sudo nano /etc/lightdm/lightdm.conf; #Above Seat configuration type: "greeter-session = io.elementary-greeter"
+sudo nano /etc/lightdm/lightdm.conf;
+
+#Above Seat configuration type: "greeter-session = io.elementary-greeter"
 
 sudo systemctl enable lightdm.service;
 
 sudo systemctl enable NetworkManager;
 
 sudo systemctl start lightdm.service;
+
+#after login
+
+git clone https://aur.archlinux.org/yay.git
+sudo chown $USERNAME:users yay
+cd yay
+makepkg -si
+
+yay -S pantheon-unstable gala-git switchboard-plug-pantheon-tweaks-git pantheon-print file-roller
+
+yay -S wingpanel-standalone-git
+
+yay -S wingpanel-indicator-ayatana-git
