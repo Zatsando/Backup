46253f8e4423b745426b6847b5a856db736dc7eb
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2022-09-26 10:37:47 +0300

    changes

--- debian-post-install.sh
+++ debian-post-install.sh
@@ -7,17 +7,13 @@ sudo add-apt-repository -y ppa:philip.scott/pantheon-tweaks;
 sudo apt install pantheon-tweaks;
 
 #swap
+swapon;
 sudo swapoff /dev/dm-2;
 rm /dev/dm-2;
 dd if=/dev/zero of=/swapfile bs=1M count=16384;
 chmod 600 /swapfile;
 mkswap /swapfile;
 swapon /swapfile;
-
-#install nala instead of apt
-echo "deb https://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
-wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
-sudo apt update && sudo apt install nala;
 
 #enable wayland
 sudo nano /etc/gdm3/custom.conf;
@@ -28,11 +24,11 @@ wget -nc https://dl.winehq.org/wine-builds/winehq.key &&
 sudo mv winehq.key /usr/share/keyrings/winehq-archive.key &&
 wget -nc https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources &&
 sudo mv winehq-jammy.sources /etc/apt/sources.list.d/ &&
-sudo nala update &&
-sudo nala install --install-recommends winehq-devel -y;
+sudo apt update &&
+sudo apt install --install-recommends winehq-devel -y;
 
 #flatpaks
-flatpak install flathub net.lutris.Lutris -y;flatpak install flathub com.github.bluesabre.darkbar -y;flatpak install flathub com.spotify.Client -y;flatpak install flathub org.telegram.desktop -y;flatpak install flathub com.chatterino.chatterino -y;flatpak install flathub com.anydesk.Anydesk -y;flatpak install flathub io.github.Figma_Linux.figma_linux -y;flatpak install flathub com.usebottles.bottles -y;flatpak install flathub org.onlyoffice.desktopeditors -y;flatpak install flathub org.signal.Signal -y;flatpak install flathub com.stremio.Stremio -y;flatpak install flathub com.heroicgameslauncher.hgl -y;flatpak install flathub com.brave.Browser -y;flatpak install flathub org.qbittorrent.qBittorrent -y;flatpak install flathub org.kde.krita -y;flatpak install flathub com.discordapp.Discord -y;flatpak install flathub com.valvesoftware.Steam -y;flatpak install flathub com.visualstudio.code -y;flatpak install flathub org.videolan.VLC -y;flatpak install flathub org.mozilla.Thunderbird -y;flatpak install flathub org.mozilla.firefox -y;
+flatpak install flathub com.github.bluesabre.darkbar -y;flatpak install flathub org.telegram.desktop -y;flatpak install flathub com.usebottles.bottles -y;flatpak install flathub org.onlyoffice.desktopeditors -y;flatpak install flathub org.signal.Signal -y;flatpak install flathub com.stremio.Stremio -y;flatpak install flathub com.brave.Browser -y;flatpak install flathub org.qbittorrent.qBittorrent -y;flatpak install flathub org.kde.krita -y;flatpak install flathub com.discordapp.Discord -y;flatpak install flathub com.valvesoftware.Steam -y;flatpak install flathub com.visualstudio.code -y;flatpak install flathub org.videolan.VLC -y;flatpak install flathub org.mozilla.Thunderbird -y;flatpak install flathub org.mozilla.firefox -y;
 
 #nodejs lts
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash;
@@ -40,8 +36,12 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash;
 nvm install --lts;
 
 #remove libre office
-sudo nala remove --purge libreoffice* -y;
-sudo nala autoremove && sudo nala autoclean -y;
+sudo apt remove --purge libreoffice* -y;
+sudo apt autoremove && sudo apt autoclean -y;
+
+#native packages
+sudo apt update;
+sudo apt install protonvpn libwingpanel-dev indicator-application libglib2.0-dev libgranite-dev libindicator3-dev gnome-shell-extension-manager gnome-shell-extension-appindicator gir1.2-appindicator3-0.1 wireguard synaptic android-tools-adb android-tools-fastboot streamlink python3 git gnome-tweaks ubuntu-restricted-extras neofetch openjdk-11-jdk -y;
 
 #other apps apt
 wget https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb;
@@ -59,13 +59,9 @@ cp /etc/xdg/autostart/indicator-application.desktop ~/.config/autostart/;
 sed -i 's/^OnlyShowIn.*/OnlyShowIn=Unity;GNOME;Pantheon;/' ~/.config/autostart/indicator-application.desktop;
 sudo nano /etc/xdg/autostart/indicator-application.desktop;
 
-#native packages
-sudo apt update;
-sudo apt install protonvpn libwingpanel-dev indicator-application libglib2.0-dev libgranite-dev libindicator3-dev gnome-shell-extension-manager gnome-shell-extension-appindicator gir1.2-appindicator3-0.1 wireguard synaptic android-tools-adb android-tools-fastboot streamlink python3 git gnome-tweaks ubuntu-restricted-extras neofetch openjdk-11-jdk -y;
-
 #java
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64; 
 export PATH=$PATH:$JAVA_HOME/bin;
 
 #final
-sudo nala autoremove;
+sudo apt autoremove;
