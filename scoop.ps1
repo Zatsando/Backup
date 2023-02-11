b0a3a3e3dbd263b8064f7c2d4df1e6dc0a86e907
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2023-02-11 21:07:32 +0000

    Update scoop.ps1

--- scoop.ps1
+++ scoop.ps1
@@ -7,10 +7,10 @@ scoop bucket add versions;
 scoop bucket add java;
 scoop bucket add games;
 
-scoop install ungoogled-chromium sharex android-studio obs-studio vlc discord steam vscode signal temurin17-jdk temurin17-jre krita 7zip figma nvm qbittorrent notepadplusplus stremio mailspring streamlink python librewolf onlyoffice-desktopeditors kdeconnect quick-picture-viewer vncviewer eartrumpet bulk-crap-uninstaller rustdesk nextcloud
+scoop install brave sharex android-studio obs-studio vlc discord steam vscode signal temurin17-jdk temurin17-jre krita 7zip figma nvm qbittorrent notepadplusplus stremio mailspring streamlink python librewolf onlyoffice-desktopeditors kdeconnect quick-picture-viewer vncviewer eartrumpet bulk-crap-uninstaller rustdesk nextcloud
 
 wget https://github.com/valinet/ExplorerPatcher/releases/download/22621.608.51.6_d262c41/ep_setup.exe -o ep_setup.exe;
-wget https://blitz.gg/download/win -o blitz.exe
+wget https://github.com/cangzhang/champ-r/releases/download/v1.3.11/win-unpacked.zip -o champr.zip;
 wget https://content-system.gog.com/open_link/download?path=/open/galaxy/client/setup_galaxy_1.2.67.58.exe -o setup_galaxy_1.2.67.58.exe;
 
 # Brave
