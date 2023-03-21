e5f40651f7ec6a6b8ca5393b24ea0c1d30db7aec
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2023-03-21 16:09:13 +0000

    Update scoop.ps1

--- scoop.ps1
+++ scoop.ps1
@@ -7,14 +7,9 @@ scoop bucket add versions;
 scoop bucket add java;
 scoop bucket add games;
 
-scoop install brave sharex android-studio obs-studio vlc discord steam vscode signal temurin17-jdk temurin17-jre krita 7zip figma nvm qbittorrent notepadplusplus stremio mailspring streamlink python librewolf onlyoffice-desktopeditors kdeconnect quick-picture-viewer vncviewer eartrumpet bulk-crap-uninstaller rustdesk nextcloud
+scoop install sharex android-studio obs-studio vlc discord steam vscode signal temurin17-jdk temurin17-jre krita 7zip figma nvm qbittorrent notepadplusplus stremio mailspring streamlink python onlyoffice-desktopeditors kdeconnect quick-picture-viewer vncviewer eartrumpet bulk-crap-uninstaller rustdesk nextcloud
 
-wget https://github.com/valinet/ExplorerPatcher/releases/download/22621.608.51.6_d262c41/ep_setup.exe -o ep_setup.exe;
-wget https://github.com/cangzhang/champ-r/releases/download/v1.3.11/win-unpacked.zip -o champr.zip;
-wget https://content-system.gog.com/open_link/download?path=/open/galaxy/client/setup_galaxy_1.2.67.58.exe -o setup_galaxy_1.2.67.58.exe;
-
-# Brave
-New-Item -ItemType Junction -Path "$env:AppData/../Local/BraveSoftware/Brave-Browser/User Data" -Target "$env:USERPROFILE\scoop\persist\brave\User Data"
-
-#ungoogled-chromium
-New-Item -ItemType Junction -Path "$env:AppData/../Local/Chromium/User Data" -Target "$env:USERPROFILE\scoop\persist\ungoogled-chromium\User Data"
+winget install -e --id Brave.Brave;
+winget install -e --id LibreWolf.LibreWolf;
+winget install -e --id valinet.ExplorerPatcher;
+winget install -e --id cangzhang.champ-r;
