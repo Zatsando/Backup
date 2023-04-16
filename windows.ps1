11abe9f4871886fd971b9671f922aada6ef71625
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2023-04-16 12:44:02 +0300

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -10,13 +10,16 @@ scoop bucket add versions;
 scoop bucket add java;
 scoop bucket add games;
 
-scoop install winget sharex nodejs-lts android-studio obs-studio vlc discord steam vscode signal temurin-lts-jdk krita 7zip figma qbittorrent notepadplusplus stremio mailspring streamlink python libreoffice kdeconnect quick-picture-viewer vncviewer eartrumpet bulk-crap-uninstaller rustdesk nextcloud putty heroic-games-launcher playnite fastfetch nano
+scoop install brave winget sharex nodejs-lts android-studio obs-studio vlc discord steam vscode signal temurin-lts-jdk krita 7zip figma qbittorrent notepadplusplus stremio mailspring streamlink python libreoffice kdeconnect quick-picture-viewer vncviewer eartrumpet bulk-crap-uninstaller rustdesk nextcloud putty heroic-games-launcher playnite fastfetch nano
 
-winget install -e --id Brave.Brave;
 winget install -e --id LibreWolf.LibreWolf;
 winget install -e --id valinet.ExplorerPatcher;
 winget install -e --id cangzhang.champ-r;
 
+#admin now
+irm christitus.com/win | iex
+
+## optional
 # Disable feature updates
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "DeferFeatureUpdates" -Value 1 -Type DWORD
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "DeferFeatureUpdatesPeriodInDays" -Value 365 -Type DWORD
