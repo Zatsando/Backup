19addbab24ef0accb68dbb9670741493e4d410ff
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2023-05-22 13:34:16 +0300

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -17,13 +17,13 @@ scoop bucket add games;
 scoop bucket add nonportable;
 
 #install scoop packages
-scoop install flameshot firefox brave winget nodejs-lts android-studio obs-studio vlc discord steam vscode signal temurin-lts-jdk krita 7zip qbittorrent notepadplusplus stremio mailspring streamlink python libreoffice kdeconnect eartrumpet bulk-crap-uninstaller nextcloud putty heroic-games-launcher playnite fastfetch rustdesk nano sudo ani-cli mpv fzf;
+scoop install nilesoft-shell context-menu-manager flameshot firefox brave winget nodejs-lts android-studio obs-studio vlc discord steam vscode signal temurin-lts-jdk krita 7zip qbittorrent notepadplusplus stremio mailspring streamlink python libreoffice kdeconnect eartrumpet bulk-crap-uninstaller nextcloud putty heroic-games-launcher playnite fastfetch rustdesk nano sudo ani-cli mpv fzf;
 
 #brave fix
 New-Item -ItemType Junction -Path "$env:AppData/../Local/BraveSoftware/Brave-Browser/User Data" -Target "$env:USERPROFILE\scoop\persist\brave\User Data"
 
 #install np scoops
-sudo scoop install parsec-np wireguard-np;
+sudo scoop install open-shell-np parsec-np wireguard-np;
 
 #open with admin for this
 irm christitus.com/win | iex
