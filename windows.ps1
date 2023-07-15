fb4ac49e468972fe188c22f82a966b17466c3ddd
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2023-07-16 02:30:13 +0300

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -10,25 +10,28 @@ git config --global user.name "zachvlat"
 git config --global user.email "zachariasvlatakis@gmail.com"
 
 #add buckets
-scoop bucket add extras;
-scoop bucket add versions;
-scoop bucket add java;
-scoop bucket add games;
-scoop bucket add nonportable;
+scoop bucket add extras
+scoop bucket add versions
+scoop bucket add java
+scoop bucket add games
+scoop bucket add nonportable
 
 #install scoop packages
-scoop install vcredist2022 windowsdesktop-runtime vcredist2013 aria2 obsidian scrcpy openrgb chatterino legendary thunderbird nilesoft-shell context-menu-manager flameshot firefox brave winget nodejs-lts android-studio obs-studio vlc discord steam vscode signal temurin-lts-jdk krita 7zip qbittorrent notepadplusplus stremio streamlink python libreoffice kdeconnect eartrumpet bulk-crap-uninstaller putty heroic-games-launcher playnite fastfetch rustdesk nano sudo ani-cli mpv fzf adb;
+scoop install vcredist2022 windowsdesktop-runtime vcredist2013 aria2 obsidian scrcpy openrgb chatterino legendary thunderbird context-menu-manager flameshot firefox brave winget nodejs-lts android-studio obs-studio vlc discord steam vscode signal temurin-lts-jdk krita 7zip qbittorrent notepadplusplus stremio streamlink python libreoffice kdeconnect eartrumpet bulk-crap-uninstaller putty heroic-games-launcher playnite fastfetch rustdesk nano sudo ani-cli mpv fzf adb
 
 #brave fix
 New-Item -ItemType Junction -Path "$env:AppData/../Local/BraveSoftware/Brave-Browser/User Data" -Target "$env:USERPROFILE\scoop\persist\brave\User Data"
 
 #install np scoops
-sudo scoop install open-shell-np parsec-np wireguard-np virtualbox-np;
+sudo scoop install wireguard-np virtualbox-np
 
 #wget
 wget https://github.com/cangzhang/champ-r/releases/download/v2.0.2-b7/champr-v2.0.2-b7.zip -o champr.zip
-https://proton.me/download/drive/windows/Proton%20Drive%20Setup%201.3.6.exe -o drive.exe
+wget https://proton.me/download/drive/windows/Proton%20Drive%20Setup%201.3.6.exe -o drive.exe
 ./drive.exe
+
+#classic context menu
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 
 #open with admin for this
 irm christitus.com/win | iex
